2ae2139cc38ff40a2d642ce8181760a4db41efad
njiru brian <muriithibrian283@gmail.com>
2021-10-21 22:35:53 +0300

    Did the jvascript bit

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,2 +1,23 @@
-alert ("You have no other choice but to on cookies. Is that okay or should we block you off");
 
+
+function bmiCalculator (){
+    var weight= document.getElementById("uzito").value;
+    var weight =parseInt(weight);
+    var height= document.getElementById("urefu").value;
+    var height = parseFloat(height);
+    var BMI = Math.floor(weight/(height*height));
+   
+
+    
+    if (BMI < 18){
+        alert("underweight");
+    } else if (BMI >19 && BMI< 25){
+        alert("fit");
+    } else if (BMI > 26 && BMI < 29){
+        alert("overweight");
+    } else{
+        alert("obese");
+    }
+
+}
+document.querySelector("button").addEventListener("click", bmiCalculator);
